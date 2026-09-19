d32a27d2ced6f188c2042561cce479d5abc3a3dc
Tzvika <maizlin92@gmail.com>
2026-09-19 03:23:35 +0300

    add url links, specify language, limit articles, add subject

--- send_email.py
+++ send_email.py
@@ -1,18 +1,19 @@
 import smtplib, ssl
+import os
 
+email_address = ''
+email_password = ''
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
-    username = "cielush2510@gmail.com"
-    password = "ygxbauweewzdiutm"
+    username = email_address
+    password = email_password
 
-    receiver = "cielush2510@gmail.com"
+    receiver = email_address
     context = ssl.create_default_context()
+
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
-
-if __name__ == "__main__":
-    send_email("Hello World")
